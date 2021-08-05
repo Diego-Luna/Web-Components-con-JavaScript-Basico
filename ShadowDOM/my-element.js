984b2263b1a30b0f060c822ed1392d42335584be
Dieg Luna <diegofcolunalopez@gmail.com>
2021-08-04 20:43:43 -0500

    Shadow DOM

---
+++ ShadowDOM/my-element.js
@@ -0,0 +1,50 @@
+// creamos la case para nuestra etiqueta de HTML
+class myElement extends HTMLElement {
+  constructor() {
+    super();
+    // ponemos Shadow
+    this.attachShadow({ mode: "open" });
+  }
+
+  // creamos una funcion que manejara las etiquetas HTML
+  getTemplate() {
+    // agregamos nuestro template
+    const template = document.createElement("template");
+    template.innerHTML = `
+    <section>
+      <h2 class="title" > Hola mundo </h2>
+      <div>
+        <p>Soy mas texto ejemplo</p>
+      </div>
+    </section>
+
+    ${this.getStyles()}
+`;
+
+    return template;
+  }
+
+  getStyles() {
+    return `
+      <style>
+      h2{
+        color: red;
+      }
+      </style>
+    `;
+  }
+
+  // Creamos una funcion que nos agrega a nuestra aplicacion
+  render() {
+    // agregamos a shadowRoot, nuestro template
+    this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
+  }
+
+  // hacemos que se auto ejecute
+  connectedCallback() {
+    this.render();
+  }
+}
+
+// Conectamos nuestra etiqueta HTML, con nuestra clase
+customElements.define("my-element", myElement);
